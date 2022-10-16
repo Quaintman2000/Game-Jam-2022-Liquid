eb7ac90261f27e2ef10652a230f30e6e9293cdea
Alec Carter <alec.g.carter@gmail.com>
2022-10-16 02:19:37 -0700

    Made cool particle Effects

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -20,35 +20,48 @@ public class Cauldron : MonoBehaviour
     [SerializeField] RectTransform _bookUIObject;
 
     List<PotionType> _potionsAdded = new List<PotionType>();
+    Vector3 __originalPosition;
+    Vector3 __originalScale;
+
+    bool _isAnimating;
 
     private void Awake()
     {
         _potionsAdded.Clear();
+        __originalPosition = _discoverSpriteRenderer.transform.position;
+        __originalScale = _discoverSpriteRenderer.transform.localScale;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<PotionItem>(out PotionItem potion))
         {
-            PotionItemManager.Instance.ResetPotion(potion,false);
-            _potionsAdded.Add(potion.PotionType);
-            if(_potionsAdded.Count == _numOfPotionsToMakeCombo)
+            if (!_isAnimating)
             {
-                // TODO: Determine which monster is created.
-                MonsterData __madeMonster = DetermineMonster();
+                PotionItemManager.Instance.ResetPotion(potion, false);
+                _potionsAdded.Add(potion.PotionType);
+                if (_potionsAdded.Count == _numOfPotionsToMakeCombo)
+                {
+                    // TODO: Determine which monster is created.
+                    MonsterData __madeMonster = DetermineMonster();
 
-                if(__madeMonster == null)
-                {
-                    DudMadeEvent?.Invoke();
+                    if (__madeMonster == null)
+                    {
+                        DudMadeEvent?.Invoke();
+                    }
+                    else
+                    {
+                        MonsterMadeEvent?.Invoke();
+                        StartCoroutine(PlayReveilAnimation(__madeMonster));
+                        _book.DiscoverMonster(__madeMonster);
+                    }
+                    _potionsAdded.Clear();
+                    PotionItemManager.Instance.RevealAllPotions();
                 }
-                else
-                {
-                    MonsterMadeEvent?.Invoke();
-                    PlayReveilAnimation(__madeMonster);
-                    _book.DiscoverMonster(__madeMonster);
-                }
-                _potionsAdded.Clear();
-                PotionItemManager.Instance.RevealAllPotions();
+            }
+            else
+            {
+                PotionItemManager.Instance.ResetPotion(potion, true);
             }
         }
     }
@@ -65,14 +78,15 @@ public class Cauldron : MonoBehaviour
         return null;
     }
 
-    public async void PlayReveilAnimation(MonsterData monster)
+       
+    IEnumerator PlayReveilAnimation(MonsterData monster)
     {
-        var __originalPosition = _discoverSpriteRenderer.transform.position;
+        _isAnimating = true;
 
         _discoverSpriteRenderer.gameObject.SetActive(true);
         var __moveToPosition = Camera.main.ScreenToWorldPoint(_bookUIObject.position + (Vector3.forward * 10));
 
-        var __originalScale = _discoverSpriteRenderer.transform.localScale;
+        
         _discoverSpriteRenderer.sprite = monster.MonsterImage;
 
         var __distance = Vector2.Distance(_discoverSpriteRenderer.transform.position, __moveToPosition);
@@ -83,7 +97,7 @@ public class Cauldron : MonoBehaviour
         while(__showTimeLeft >= 0)
         {
             __showTimeLeft -= Time.deltaTime;
-            await Task.Yield();
+            yield return null;
         }
 
         var __startTime = Time.time;
@@ -93,12 +107,14 @@ public class Cauldron : MonoBehaviour
             _discoverSpriteRenderer.transform.position += (__moveToPosition - _discoverSpriteRenderer.transform.position) * __speed * Time.deltaTime;
             _discoverSpriteRenderer.transform.localScale = __originalScale * _animationCurve.Evaluate(Time.time - __startTime);
             __timeLeft -= Time.deltaTime;
-            await Task.Yield();
+            yield return null;
         }
 
         _discoverSpriteRenderer.gameObject.SetActive(false);
         _discoverSpriteRenderer.transform.position = __originalPosition;
         _discoverSpriteRenderer.transform.localScale = __originalScale;
+
+        _isAnimating = false;
     }
 
 }
